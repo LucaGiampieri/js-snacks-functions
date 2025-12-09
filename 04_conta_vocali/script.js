f2459bbcaf2ba4c52c47def94baeb324d96fea61
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-09 16:35:06 +0100

    Esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,17 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelsCounter (myWord){
+    let counter = 0;
+    for (let i = 0; i < myWord.length; i++)
+        if(myWord[i] === 'a' || myWord[i] === 'e' || myWord[i] === 'i' || myWord[i] === 'o' || myWord[i] === 'u' || myWord[i] === 'A' || myWord[i] === 'E' || myWord[i] === 'I' || myWord[i] === 'O' || myWord[i] === 'U'){
+            counter = counter +1;
+        }
+        return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(vowelsCounter(word));
+console.log(vowelsCounter('aBCdEfghI'));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
