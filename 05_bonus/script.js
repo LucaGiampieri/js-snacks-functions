9c396ec438b52efcc6830fcad2ecbc8449bba85b
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-09 17:05:21 +0100

    Esercizio 5 Bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,28 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const userNamePrompt = prompt('Inserisci il tuo nome');
 
 
 // Dichiara la funzione qui.
 
+const hourlyGreeting = (userName) =>{
+    const now = new Date();
+    const hour = now.getHours(); 
+
+    if ( hour >= 5 && hour < 13 ) {
+        return `Buongiorno ${userName}`;
+    } else if (hour >=13 && hour < 17) {
+        return `Buon pomeriggio ${userName}`;
+    } else {
+        return `Buona sera ${userName}`;
+}
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(hourlyGreeting(name));
+console.log(hourlyGreeting(userNamePrompt));
 
 
 
-//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario
