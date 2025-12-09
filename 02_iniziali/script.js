09e4beb8e6bf167c7b4b4e40e6508a90559be026
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-09 15:58:07 +0100

    Esercizio 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initialLetter(myArray){
+    const initialLetterArray = [];
+    for(let i = 0; i < myArray.length; i++){
+        initialLetterArray.push(myArray[i].charAt(0));
+    }
+    return initialLetterArray;
+}
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(initialLetter(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
